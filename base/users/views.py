682c4dfc8d226500caf78904a691ab6569e09c09
jana <rathan.kparam@gmail.com>
2020-12-26 23:02:21 +0800

    Get the api working

--- base/users/views.py
+++ base/users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import User
+
+@csrf_exempt
+def get_all_users(request, *args, **kwargs):
+    print('-----------------get_all_users')
+    users = User.objects.all()
+    json = []
+    for u in users:
+        json.append({'name': u.name, 'profession': u.profession})
+
+    return JsonResponse(json, safe=False)
