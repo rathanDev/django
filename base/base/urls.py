06d1a67605127435093bde75cb1e1c248013c4bf
jana <rathan.kparam@gmail.com>
2020-12-26 22:24:54 +0800

    Init

--- base/base/urls.py
+++ base/base/urls.py
@@ -1,4 +1,4 @@
-"""djangojwt URL Configuration
+"""base URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,11 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
-
-from .views import UserRegistrationView
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^signup', UserRegistrationView.as_view()),
 ]
