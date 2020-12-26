174425415629b98e34262dd2844780351ffb3551
jana <rathan.kparam@gmail.com>
2020-12-26 23:31:45 +0800

    Get the serializer api working

--- base/customers/urls.py
+++ base/customers/urls.py
@@ -3,6 +3,23 @@ from django.urls import path, include
 
 from . import views
 
+# -----
+
+from rest_framework import routers
+
+
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+
+
 urlpatterns = [
     path('all/', views.get_all_customers, name='get_all_customers'),
+
+    # -----
+
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- base/customers/views.py
+++ base/customers/views.py
@@ -4,6 +4,16 @@ from django.views.decorators.csrf import csrf_exempt
 
 from .models import Customer
 
+# -----
+
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+
+from .serializers import UserSerializer, GroupSerializer
+
+
+
 @csrf_exempt
 def get_all_customers(request, *args, **kwargs):
     print('------------------------------------get_all_customers')
@@ -13,3 +23,16 @@ def get_all_customers(request, *args, **kwargs):
         json.append({'name': c.name, 'profession': c.profession})
 
     return JsonResponse(json, safe=False)
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
