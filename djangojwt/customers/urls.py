2f13048ae44e833ac2538d525b8776c14d0dc44f
jana <rathan.kparam@gmail.com>
2020-12-27 09:33:49 +0800

    Add rest_frameworks app to settings.py

--- djangojwt/customers/urls.py
+++ djangojwt/customers/urls.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import routers
+
 from . import views
+
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
 
 urlpatterns = [
     path('all/', views.get_all_customers, name='get_all_customers'),
+
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- djangojwt/customers/views.py
+++ djangojwt/customers/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 from django.http import JsonResponse
 
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+
+from .serializers import UserSerializer, GroupSerializer
+
 from .models import Customer
 
 
@@ -14,3 +20,25 @@ def get_all_customers(request, *args, **kwargs):                        # http:/
         json.append({'name': c.name, 'profession': c.profession})
 
     return JsonResponse(json, safe=False)
+
+
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    print('UserViewSet------------------------------------------')
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+
+
+
+
