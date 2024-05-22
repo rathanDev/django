6dfb172c68c043c7f7ffe45cfd1449a7632bbd1e
Jana <rathan.kparam@gmail.com>
2024-05-22 08:19:00 +0800

    Use html template

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# request --> response
+# request handler 
+
+    # Pull data from db
+    # Transform 
+    # Send email
+
+def say_hello(request):
+    # return HttpResponse('Hello World')
+    return render(request, 'hello.html')
